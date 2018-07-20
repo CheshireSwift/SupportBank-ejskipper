14bf3163570aaa79a1065af5470721e1865d4166
Sam Collard <swift@swifteagle.co.uk>
2018-07-20 13:41:44 +0100

    Delete all

--- parseIntTransacAmounts.js
+++
@@ -1,9 +0,0 @@
-function parseIntTransacAmounts(allTransactions) {
-    for (let i=0;i<allTransactions.length;i++) {
-        allTransactions[i].amountPounds=parseInt(allTransactions[i].amountPounds)
-        allTransactions[i].amountPennies=parseInt(allTransactions[i].amountPennies)
-    }
-}
-
-
-module.exports={parseIntTransacAmounts}
